480b2c2848369083a8015ffac2f77ff398a566c8
Dima <Uchiha132132qqq@gmail.com>
2023-02-26 21:16:34 +0300

    styles for sorting page

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { Button } from "../ui/button/button";
+import { RadioInput } from "../ui/radio-input/radio-input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styles from "./sorting-page.module.css";
+
+const randomArr = () => {
+  
+}
 
 export const SortingPage: React.FC = () => {
+
   return (
     <SolutionLayout title="Сортировка массива">
-
+      <div className={styles.header}>
+        <div className={styles.header__radioInputs}>
+          <RadioInput label="Выбор" />
+          <RadioInput label="Пузырёк" />
+        </div>
+        <div className={styles.header__sortButtons}>
+          <Button text="По возрастанию" />
+          <Button text="По убыванию" />
+        </div>
+        <Button text="Новый массив" />
+      </div>
     </SolutionLayout>
   );
 };
