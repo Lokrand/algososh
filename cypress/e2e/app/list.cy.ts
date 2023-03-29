31d93d6f902a34cbcd2481ccfc77cd9b70e81f19
Dima <Uchiha132132qqq@gmail.com>
2023-03-29 21:27:03 +0300

    add list tests

--- cypress/e2e/app/list.cy.ts
+++ cypress/e2e/app/list.cy.ts
@@ -1,5 +1,10 @@
 /* eslint-disable cypress/no-unnecessary-waiting */
-import { DELAY_IN_MS, stylesChanging, stylesDefault, stylesModified } from "./app.cy";
+import {
+  DELAY_IN_MS,
+  stylesChanging,
+  stylesDefault,
+  stylesModified,
+} from "./app.cy";
 
 // Здесь для тестирования я использовал библиотеку cypress-react-selector, т.к. по другому не получалось.
 describe("Testing <<List>> page", function() {
@@ -18,113 +23,167 @@ describe("Testing <<List>> page", function() {
     cy.get("#add-by-index").should("be.disabled");
     cy.get("#remove-by-index").should("be.disabled");
     cy.get("#li")
-    .children()
-    .should("have.length", 4);
+      .children()
+      .should("have.length", 4);
   });
-  
+
   // it("should add element in head", function() {
-    //   cy.get("#list-input-string").type("cat");
-    //   cy.get("#tail-add").should("not.be.disabled");
-    //   cy.get("#head-add").should("not.be.disabled").click();
-    //   cy.get("#spinner").should("exist");
-    
-    //   cy.react("Circle")
-    //   .should("have.length", 5)
-    //   .last()
-    //   .should("have.text", "cat")
-    //   .should("have.css", "width", "56px")
-    
-    //   cy.wait(DELAY_IN_MS);
-    
-    //   cy.react("Circle")
-    //   .children()
-    //   .first()
-    //   .should("have.text", "head")
-    //   .next()
-    //   .should("have.text", "cat")
-    //   .should("have.css", "width", "80px")
-    //   .should("have.css", "border", stylesModified)
-    //   .next()
-    //   .should("have.text", "0")
-    
-    //   cy.wait(DELAY_IN_MS);
-    
-    //   cy.react("Circle")
-    //   .children()
-    //   .first()
-    //   .should("have.text", "head")
-    //   .next()
-    //   .should("have.text", "cat")
-    //   .should("have.css", "width", "80px")
-    //   .should("have.css", "border", stylesDefault)
-    //   .next()
-    //   .should("have.text", "0")
-    
-    //   cy.get("#list-input-string").should("be.empty");
-    //   cy.get("#tail-add").should("be.disabled");
-    //   cy.get("#head-add").should("be.disabled");
-    //   cy.get("#spinner").should("not.exist");
-    // });
-    
-    // it("should add element in tail", function() {
-      //   cy.get("#list-input-string").type("cat");
-      //   cy.get("#head-add").should("not.be.disabled");
-    //   cy.get("#tail-add").should("not.be.disabled").click();
-    //   cy.get("#spinner").should("exist");
-    
-    //   cy.react("Circle")
-    //   .should("have.length", 5)
-    //   .last()
-    //   .should("not.have.text", "tail")
-    //   .should("have.text", "cat")
-    //   .should("have.css", "width", "56px")
-    
-    //   cy.wait(DELAY_IN_MS);
-    
-    //   cy.react("Circle")
-    //   .children()
-    //   .last()
-    //   .should("have.text", "head")
-    //   // .nextUntil()
-    //   .should("have.text", "cat")
-    //   .should("have.css", "width", "80px")
-    //   .should("have.css", "border", stylesModified)
-    //   .next()
-    //   .should("have.text", "0")
-    
-    //   cy.wait(DELAY_IN_MS);
-    
-    //   cy.react("Circle")
-    //   .children()
-    //   .first()
-    //   .should("have.text", "head")
-    //   .next()
-    //   .should("have.text", "cat")
-    //   .should("have.css", "width", "80px")
-    //   .should("have.css", "border", stylesDefault)
-    //   .next()
-    //   .should("have.text", "0")
-    
-    //   cy.get("#list-input-string").should("be.empty");
-    //   cy.get("#tail-add").should("be.disabled");
-    //   cy.get("#head-add").should("be.disabled");
-    //   cy.get("#spinner").should("not.exist");
-    // });
-    // });
-    // it("should add element by index", function() {});
-    it("should remove element in head", function() {
-      cy.get("#li").children().should("have.length", 4)
-      cy.get("#head-remove").click();
-      cy.get("#li").children().each((el, i) => {
-        
-      })
-      // cy.react("Circle").children().first().should("have.text", "");
-      cy.wait(DELAY_IN_MS);
-      cy.get("#li").children().should("have.length", 3)
-    
-      
+  //     cy.get("#list-input-string").type("cat");
+  //     cy.get("#tail-add").should("not.be.disabled");
+  //     cy.get("#head-add").should("not.be.disabled").click();
+  //     cy.get("#spinner").should("exist");
+
+  //     cy.react("Circle")
+  //     .should("have.length", 5)
+  //     .last()
+  //     .should("have.text", "cat")
+  //     .should("have.css", "width", "56px")
+
+  //     cy.wait(DELAY_IN_MS);
+
+  //     cy.react("Circle")
+  //     .children()
+  //     .first()
+  //     .should("have.text", "head")
+  //     .next()
+  //     .should("have.text", "cat")
+  //     .should("have.css", "width", "80px")
+  //     .should("have.css", "border", stylesModified)
+  //     .next()
+  //     .should("have.text", "0")
+
+  //     cy.wait(DELAY_IN_MS);
+
+  //     cy.react("Circle")
+  //     .children()
+  //     .first()
+  //     .should("have.text", "head")
+  //     .next()
+  //     .should("have.text", "cat")
+  //     .should("have.css", "width", "80px")
+  //     .should("have.css", "border", stylesDefault)
+  //     .next()
+  //     .should("have.text", "0")
+
+  //     cy.get("#list-input-string").should("be.empty");
+  //     cy.get("#tail-add").should("be.disabled");
+  //     cy.get("#head-add").should("be.disabled");
+  //     cy.get("#spinner").should("not.exist");
+  //   });
+
+  // it("should remove element in head", function() {
+  //   cy.react("Circle").should("have.length", 4)
+  //   cy.get("#head-remove").click();
+  //   cy.get("[data-testid='head']").should("exist");
+  //   cy.wait(DELAY_IN_MS)
+  //   cy.get("[data-testid='head']").should("have.text", "head");
+  //   cy.react("Circle").should("have.length", 3);
+  // });
+
+  // it("should remove element in tail", function() {
+  //   cy.react("Circle").should("have.length", 4)
+  //   cy.get("#tail-remove").click();
+  //   cy.get("[data-testid='tail']").should("exist");
+  //   cy.wait(DELAY_IN_MS)
+  //   cy.get("[data-testid='tail']").should("have.text", "tail");
+  //   cy.react("Circle").should("have.length", 3);
+  // });
+
+  //========================
+
+  // it("should add element in tail", function() {
+  //     cy.get("#list-input-string").type("cat");
+  //     cy.get("#head-add").should("not.be.disabled");
+  //   cy.get("#tail-add").should("not.be.disabled").click();
+  //   cy.get("#spinner").should("exist");
+
+  //   cy.react("Circle")
+  //   .should("have.length", 5)
+  //   .last()
+  //   .should("not.have.text", "tail")
+  //   .should("have.text", "cat")
+  //   .should("have.css", "width", "56px")
+
+  //   cy.wait(DELAY_IN_MS);
+
+  //   cy.react("Circle")
+  //   .children()
+  //   .last()
+  //   .should("have.text", "head")
+  //   // .nextUntil()
+  //   .should("have.text", "cat")
+  //   .should("have.css", "width", "80px")
+  //   .should("have.css", "border", stylesModified)
+  //   .next()
+  //   .should("have.text", "0")
+
+  //   cy.wait(DELAY_IN_MS);
+
+  //   cy.react("Circle")
+  //   .children()
+  //   .first()
+  //   .should("have.text", "head")
+  //   .next()
+  //   .should("have.text", "cat")
+  //   .should("have.css", "width", "80px")
+  //   .should("have.css", "border", stylesDefault)
+  //   .next()
+  //   .should("have.text", "0")
+
+  //   cy.get("#list-input-string").should("be.empty");
+  //   cy.get("#tail-add").should("be.disabled");
+  //   cy.get("#head-add").should("be.disabled");
+  //   cy.get("#spinner").should("not.exist");
+  // });
+
+  it("should add element by index", function() {
+    cy.get("#list-input-number").type("1");
+    cy.get("#remove-by-index").should("not.be.disabled");
+    cy.get("#list-input-string").type("cat");
+    cy.get("#add-by-index")
+      .should("not.be.disabled")
+      .click();
+    cy.get("#spinner").should("exist");
+    cy.get("[data-testid='head']")
+      .should("exist")
+      .should("have.text", "cat");
+    cy.wait(DELAY_IN_MS);
+    cy.get("#li")
+      .children()
+      .eq(1)
+      .within(() => {
+        cy.get("[data-testid='head']").should("exist");
+      });
+    cy.react("Circle")
+      .children()
+      .eq(1)
+      .should("have.css", "border", stylesChanging);
+    cy.wait(DELAY_IN_MS);
+    cy.get("#li")
+      .children()
+      .eq(1)
+      .within(() => {
+        cy.get("[data-testid='head']").should("not.exist");
+      });
+    cy.react("Circle")
+      .eq(1)
+      .children()
+      .should("have.css", "border", stylesModified);
+    cy.get("#li")
+      .children()
+      .should("have.length", 5);
+    cy.wait(DELAY_IN_MS);
+    cy.react("Circle").each((el, i) => {
+      if (i === 0)
+        expect(el.children()[1]).to.have.css("border", stylesDefault);
+      else expect(el.children()).to.have.css("border", stylesDefault);
     });
-    // it("should remove element in tail", function() {});
-    // it("should remove element by index", function() {});
+    cy.get("#list-input-string").should("be.empty");
+    cy.get("#list-input-number").should("be.empty");
+    cy.get("#add-by-index").should("be.disabled");
+    cy.get("#remove-by-index").should("be.disabled");
+    cy.get("#spinner").should("not.exist");
   });
-  
+
+  // it("should remove element by index", function() {});
+});
